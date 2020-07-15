a4c900393a228d3c83721d2b5e6ecf0ca6cddef3
Jay T <jaytula@gmail.com>
2020-07-15 09:26:32 -0700

    TC Prize Form Wire up: 80 mins in

--- TrackerUI/CreatePrizeForm.cs
+++ TrackerUI/CreatePrizeForm.cs
@@ -18,14 +18,21 @@ namespace TrackerUI
 
         private void createPrizeButton_Click(object sender, EventArgs e)
         {
-            if(ValidateForm())
+            if (ValidateForm())
             {
-                PrizeModel model = new PrizeModel();
-                model.PlaceName = placeNameValue.Text;
-                model.PlaceNumber = placeNumberLabel.Text;
+                PrizeModel model = new PrizeModel(
+                    placeNumberValue.Text,
+                    placeNumberValue.Text,
+                    prizeAmountValue.Text,
+                    prizePercentageValue.Text
+                );
+
+                foreach (IDataConnection db in GlobalConfig.Connections)
+                {
+                    db.CreatePrize(model);
+                }
             }
         }
-
         private bool ValidateForm()
         {
             bool output = true;
@@ -43,7 +50,7 @@ namespace TrackerUI
                 output = false;
             }
 
-            if(placeNameValue.Text.Length == 0)
+            if (placeNameValue.Text.Length == 0)
             {
                 output = false;
             }
@@ -51,20 +58,20 @@ namespace TrackerUI
             decimal prizeAmount = 0;
             double prizePercentage = 0;
             bool prizeAmountValid = decimal.TryParse(prizeAmountValue.Text, out prizeAmount);
-            bool prizePercentageValid = double.TryParse(prizePercentageValue.Text, out prizePercentage)
+            bool prizePercentageValid = double.TryParse(prizePercentageValue.Text, out prizePercentage);
 
-            if(!prizeAmountValid || !prizePercentageValid)
+            if (!prizeAmountValid || !prizePercentageValid)
             {
                 output = false;
             }
 
 
-            if(prizeAmount <= 0 && prizePercentage <= 0)
+            if (prizeAmount <= 0 && prizePercentage <= 0)
             {
                 output = false;
             }
 
-            if(prizePercentage < 0 || prizePercentage > 100)
+            if (prizePercentage < 0 || prizePercentage > 100)
             {
                 output = false;
             }
