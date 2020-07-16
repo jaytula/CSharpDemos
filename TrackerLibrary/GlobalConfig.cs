f2dfdfdc1c5f54e6f5a77e29c81bfd788aa71106
Jay T <jaytula@gmail.com>
2020-07-16 13:09:05 -0700

    TC SQL Connection: 16 mins in

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TrackerLibrary.DataAccess;
 
 namespace TrackerLibrary
 {
@@ -19,7 +20,7 @@ namespace TrackerLibrary
             if (textFiles)
             {
                 // TODO - Create the Text Connection
-                TextConnection text = new TextConnection();
+                TextConnector text = new TextConnector();
                 Connections.Add(text);
             }
         }
