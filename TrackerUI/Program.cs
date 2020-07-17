ed8d58c4a8e0e21629cc897ee39fee38e276c7c8
Jay T <jaytula@gmail.com>
2020-07-17 11:18:34 -0700

    Print to Output:Debug window in IDE

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,6 +22,7 @@ namespace TrackerUI
             // Initialize the database connections
             TrackerLibrary.GlobalConfig.InitializeConections(true, true);
 
+            Debug.WriteLine("ABC");
             //Application.Run(new TournamentDashboardForm());
             Application.Run(new CreatePrizeForm());
 
