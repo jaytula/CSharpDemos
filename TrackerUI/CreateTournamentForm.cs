e036b7a96c61a60979dcb50c7b3defda6eb9f6a7
Jay T <jaytula@gmail.com>
2020-08-06 17:09:04 -0700

    TC Create Tournament Form 1: 23 mins in

--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -17,6 +17,7 @@ namespace TrackerUI
         public CreateTournamentForm()
         {
             InitializeComponent();
+            InitializeLists();
         }
 
         private void label1_Click(object sender, EventArgs e)
