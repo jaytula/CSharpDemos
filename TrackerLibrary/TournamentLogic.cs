f7eb29949729cca207e31bbc15d56e151914ad58
Jay T <jaytula@gmail.com>
2020-09-03 12:35:33 -0700

    Create Tournament Form 4: 71 mins in

--- TrackerLibrary/TournamentLogic.cs
+++ TrackerLibrary/TournamentLogic.cs
@@ -39,9 +39,16 @@ namespace TrackerLibrary
 
                     if(currMatchup.Entries.Count > 1)
                     {
-
+                        currMatchup.MatchupRound = round;
+                        currRound.Add(currMatchup);
+                        currMatchup = new MatchupModel();
                     }
                 }
+
+                model.Rounds.Add(currRound);
+                previousRound = currRound;
+                currRound = new List<MatchupModel>();
+                round += 1;
             }
         }
 
